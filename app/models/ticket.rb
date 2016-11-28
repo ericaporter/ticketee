9bf4d3eae1a6db8fa4cc09bdf2a112c9c7a11866
Erica Porter <erica@kyan.com>
2016-11-28 09:39:41 +0000

    Link tickets and users upon ticket creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   validates :name, presence: true
+  belongs_to :author, class_name: "User"
   validates :description, presence: true, length: { minimum: 10 }
 end
